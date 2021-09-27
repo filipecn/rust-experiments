20b08728867552f876380bb33067e559715eebce
filipecn <fuiripecn@gmail.com>
2021-09-27 12:19:41 -0300

    single board conway

--- conway/src/main.rs
+++ conway/src/main.rs
@@ -2,73 +2,79 @@
 //
 use std::{thread, time};
 
-fn init_board(board: &mut Vec<String>) {
+fn init_board(board: &mut Vec<Vec<char>>) {
     let n = board.len();
     let m = board[0].len();
 
     for row in 0..n {
-        let row_bytes: &mut [u8] = unsafe { board[row].as_bytes_mut() };
         for col in 0..m {
-            row_bytes[col] = '.' as u8;
+            board[row][col] = '.';
         }
     }
 
     for row in 3..6 {
-        let row_bytes: &mut [u8] = unsafe { board[row].as_bytes_mut() };
-        for col in 3..6 {
-            row_bytes[col] = 'x' as u8;
+        for col in 2..7 {
+            board[row][col] = 'x';
         }
     }
 }
 
-fn step(board: &Vec<String>, next_board: &mut Vec<String>) {
+fn is_alive(c: char) -> bool {
+    return c == 'x' || c == 'l';
+}
+
+fn step(board: &mut Vec<Vec<char>>) {
     let n = board.len();
     let m = board[0].len();
-    for i in 0..n {
-        let row_bytes: &mut [u8] = unsafe { next_board[i].as_bytes_mut() };
-        for j in 0..m {
+    // first pass to compute new state (with special characters)
+    for row in 0..n {
+        for col in 0..m {
             // count live neighbors
             let mut count = 0;
-            if board[(i + n - 1) % n].chars().nth((j + m - 1) % m).unwrap() == 'x' {
-                count += 1;
-            }
-            if board[(i + n - 1) % n].chars().nth(j).unwrap() == 'x' {
-                count += 1;
-            }
-            if board[(i + n - 1) % n].chars().nth((j + 1) % m).unwrap() == 'x' {
-                count += 1;
-            }
-            if board[(i + 1) % n].chars().nth((j + m - 1) % m).unwrap() == 'x' {
-                count += 1;
-            }
-            if board[(i + 1) % n].chars().nth(j).unwrap() == 'x' {
-                count += 1;
-            }
-            if board[(i + 1) % n].chars().nth((j + 1) % m).unwrap() == 'x' {
-                count += 1;
-            }
-            if board[i].chars().nth((j + m - 1) % m).unwrap() == 'x' {
-                count += 1;
-            }
-            if board[i].chars().nth((j + 1) % m).unwrap() == 'x' {
-                count += 1;
-            }
+            for i in 0..3 {
+                for j in 0..3 {
+                    if i == 1 && j == 1 {
+                        continue;
+                    }
+                    let r = row as i32 + i - 1;
+                    let c = col as i32 + j - 1;
 
-            // init next gen cell as dead
-            row_bytes[j] = '.' as u8;
-
-            // rule 1: any live cell with two or three live neighbors survives
-            if board[i].chars().nth(j).unwrap() == 'x' && (count == 2 || count == 3) {
-                row_bytes[j] = 'x' as u8;
+                    if 0 <= r && r < n as i32 && 0 <= c && c < m as i32 {
+                        if is_alive(board[r as usize][c as usize]) {
+                            //if is_alive(board[(row + n + i - 1) % n][(col + m + j - 1) % m]) {
+                            count += 1;
+                        }
+                    }
+                }
             }
+            // current cell value
+            let c = board[row][col];
 
-            // rule 2: any dead cell with three live neighbors becomes a live cell
-            if board[i].chars().nth(j).unwrap() == '.' && count == 3 {
-                row_bytes[j] = 'x' as u8;
+            if c == 'x' && (count == 2 || count == 3) {
+                // rule 1: any live cell with two or three live neighbors survives
+                board[row][col] = 'x';
+            } else if c == '.' && count == 3 {
+                // rule 2: any dead cell with three live neighbors becomes a live cell
+                // lets mark with 'd' cells that were dead and are now alive
+                board[row][col] = 'd';
             }
-
             // rule 3: All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-            // guaranteed by the next gen cell init
+            else if c == 'x' {
+                // lets mark with 'l' cells that were live and are now dead
+                board[row][col] = 'l';
+            } else {
+                board[row][col] = '.';
+            }
+        }
+    }
+    // second pass to replace special characters
+    for row in 0..n {
+        for col in 0..m {
+            if board[row][col] == 'l' {
+                board[row][col] = '.';
+            } else if board[row][col] == 'd' {
+                board[row][col] = 'x';
+            }
         }
     }
 }
@@ -77,29 +83,21 @@ fn main() {
     // board n x m (rows x columns)
     let n = 10;
     let m = 10;
-    let mut board_a = vec![String::with_capacity(m); n];
-    let mut board_b = vec![String::with_capacity(m); n];
-    for i in 0..n {
-        for _ in 0..m {
-            board_a[i].push('.');
-            board_b[i].push('.');
-        }
-    }
-    // init board
-    init_board(&mut board_a);
-    // run game
-    for i in 0..10 {
-        if i % 2 == 0 {
-            step(&board_a, &mut board_b);
-            for row in 0..n {
-                println!("{}\n", board_b[row]);
-            }
-        } else {
-            step(&board_b, &mut board_a);
+    let row = vec!['.'; m];
+    let mut board = vec![row; n];
 
-            for row in 0..n {
-                println!("{}\n", board_a[row]);
+    // init board
+    init_board(&mut board);
+
+    // run game
+    for _ in 0..10 {
+        step(&mut board);
+
+        for row in 0..n {
+            for col in 0..m {
+                print!("{}", board[row][col]);
             }
+            println!("");
         }
 
         print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
